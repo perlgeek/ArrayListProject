9395d9a1ee507f389e0124ece4a4990a9f21e7b7
Michael Weems <tokyodweller@gmail.com>
2014-02-11 00:53:42 -0600

    added program files

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an ArrayList and add two ints
+            ArrayList list = new ArrayList();
+            list.Add("bird");
+            list.Add("rock");
+            list.Add("jacket");
+            list.Add("shoe");
+
+            // Get Last two elements in ArrayList
+            ArrayList range = list.GetRange(2, 2);
+            //Display Elements
+            foreach (string value in range)
+            {
+                Console.WriteLine(value); // jacket, shoe
+                
+            }
+
+        }
+    }
+}
